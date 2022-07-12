06a69abfb32909c0609e5bcdf41f31aa05232642
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-07-12 01:05:30 -0500

    add menu with firebase

--- menus-world/src/pages/Menu/index.jsx
+++ menus-world/src/pages/Menu/index.jsx
@@ -1,69 +1,39 @@
+import { useEffect, useState } from "react";
+import { list as listDishes } from "../../../src/services/menus";
+
 import "./Menu.css";
 
 export default function Menu() {
+  // Local state
+  const [dishes, setDishes] = useState([]);
+
+  useEffect(() => {
+    const list = async () => {
+      const data = await listDishes();
+      const parsedDishes = Object.keys(data).map((key) => {
+        return { id: key, ...data[key] };
+      });
+
+      setDishes(parsedDishes);
+    };
+
+    list();
+  }, []);
+
+  const buildMenu = (dish, index) => (
+    <div className="card-menu" key={index}>
+      <div className="name-food">{dish.dishName}</div>
+      <p className="description-food">{dish.description}</p>
+      <p className="description-food">{dish.price}</p>
+    </div>
+  );
+
+  console.log(dishes);
   return (
     <div>
-      <nav className="slider">
-        <div className="slide-track">
-          <div className="slide">
-            <span className="slide-menu">Entradas</span>
-          </div>
-          <div className="slide">
-            <span className="slide-menu">Ensaladas</span>
-          </div>
-          <div className="slide">
-            <span className="slide-menu">Cortes</span>
-          </div>
-          <div className="slide">
-            <span className="slide-menu">Bebidas</span>
-          </div>
-          <div className="slide"></div>
-        </div>
-      </nav>
-
       <div className="mainContainer">
         <div className="container">
-          <div className="card-menu">
-            <div className="name-food">
-              AGUACHILE DE CAMARÓN TRADICIONAL O TATEMADO (180 G)
-            </div>
-            <p className="description-food">
-              Chile verde, cilantro, pepino, cebolla y limon
-            </p>
-          </div>
-
-          <div className="card-menu">
-            <div className="name-food">
-              CAZUELA DE CAMARONES AL AJILLO (180 g)
-            </div>
-            <p className="description-food">
-              Toque de mezcal, papa cambray al josper y chile guajillo
-            </p>
-          </div>
-          <div className="card-menu">
-            <div className="name-food">CAZUELA DE LENGUA (250 G)</div>
-            <p className="description-food">
-              En salsa gravy, con cilantro y cebolla cambray
-            </p>
-          </div>
-          <div className="card-menu">
-            <div className="name-food">
-              COLIFLOR ROSTIZADA (500 g) <span className="">NUEVO</span>
-            </div>
-            <p className="description-food">
-              Al josper con mantequilla clarificada y salsa de quesos con trufa
-            </p>
-          </div>
-
-          <div className="card-menu">
-            <div className="name-food">CARNITAS DE RIB EYE PRIME (200 g)</div>
-            <p className="description-food">Con guacamole y cebollas cambras</p>
-          </div>
-
-          <div className="card-menu">
-            <div className="name-food">TRIPITAS DE LECHE (180 g) </div>
-            <p className="description-food">Al ajillo con pico de gallo</p>
-          </div>
+          {dishes.map(buildMenu)}
           <div className="card-info-menu">
             <li>LA PROPINA NO ES OBLIGATORIA.</li>
             <li>
