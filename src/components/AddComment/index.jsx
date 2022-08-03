8e7c52430e85e1766b42f1db19ae288f6e5b3aa2
Luis Vera <luisvera2318@gmail.com>
2022-08-02 20:31:38 -0500

    add navbar and improve styles

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -1,8 +1,51 @@
+import './AddComment.css';
+import { useState, useEffect } from 'react';
+import CreateComments from '../Comments';
 
-export default function AddComment(){
-  return(
-    <div>
-      <button type="button">Agrega un comentario</button>
-    </div>
-  )
+export default function AddComment() {
+	const [canComment, setCanComment] = useState(false);
+	const [isLogIn, setIsLogIn] = useState(true);
+	const [text, setText] = useState('');
+
+	function createContent() {
+		if (canComment === false) {
+			return (
+				<button id="addComment" type="button" onClick={() => setCanComment(true)}>
+					Agrega un comentario
+				</button>
+			);
+		}
+		if (canComment === true) {
+			return (
+				<form id="formAddComment" action="/action_page.php">
+					<textarea
+						id="addNewComment"
+						name="newComment"
+						rows="3"
+						cols="74"
+						placeholder="Escribe tu reseña del platillo"
+						// value={text}
+						// onChange={(e) => setCanComment(e.target.value)}
+					></textarea>
+					<button
+						id="addComment"
+						type="button"
+						// onClick={(e) => handleMessage(text)}
+						>
+						Enviar comentario
+					</button>
+				</form>
+			);
+		}
+	}
+
+	const handleMessage = () => {
+		console.log(text)
+  };
+
+
+
+
+
+	return <div id="addCommentSection">{createContent()}</div>;
 }
