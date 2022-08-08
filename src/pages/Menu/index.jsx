4daef149462aeddb7d244c440b5f9ae30f95f43f
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-08-08 18:27:06 -0500

    add usestate for img

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import { list as listDishes } from "../../../src/services/menus";
 import { useNavigate } from "react-router-dom";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-
+import Card from "react-bootstrap/Card";
 
 import "./Menu.css";
 
@@ -28,59 +26,55 @@ export default function Menu() {
   }, []);
 
   const buildMenu = (dish, index) => (
-
-  <div className="containerm">
-    <div className="food-menu">
-      <div className="food" key={index}> 
+    <div className="containerm">
+      <div className="food-menu">
+        <div className="food" key={index}>
           <div className="food-image">
-            <img src="https://image.freepik.com/free-photo/front-view-delicious-meat-hamburger-with-red-tomatoes-dark-background_179666-19347.jpg" alt="Food" />
-          </div>  
-         
-            <div className="name-food-name"><h5>{dish.dishName}</h5></div>
-            <div className="name-food-descrip">{dish.description}</div>
-            <div className="name-food-price"> ${dish.price}</div>
-            
-            <div className="iconos">
-            <div class="icons8-edit" onClick={() => navigate(`edit/${dish._id}`)}></div>
-            <div class="icons8-trash" onClick={() => navigate(`delete/${dish._id}`)}></div>
-            </div>
-            
-            
-          
-       
-      </div>  
+            <img src={dish.image_Url} alt="Food" />
+          </div>
+
+          <div className="name-food-name">
+            <h5>{dish.dishName}</h5>
+          </div>
+          <div className="name-food-descrip">{dish.description}</div>
+          <div className="name-food-price"> ${dish.price}</div>
+
+          <div className="iconos">
+            <div
+              class="icons8-edit"
+              onClick={() => navigate(`edit/${dish._id}`)}
+            ></div>
+            <div
+              class="icons8-trash"
+              onClick={() => navigate(`delete/${dish._id}`)}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-
-   
-  
-  
-
   );
 
   return (
-  <Card>
-    <div>
-      <div className="mainContainer">
-        <div className="container">
-          {dishes.map(buildMenu)}
-          <div className="card-info-menu">
-            <div className="notes">
-            <p>LA PROPINA NO ES OBLIGATORIA.</p>
-            
-            <p>ACEPTAMOS PAGOS EN EFECTIVO, TARJETAS VISA, MASTER CARD Y AMERICAN EXPRESS.</p>
-            
-            <p>EL PAGO CON TARJETA NO GENERA NINGUNA COMISIÓN.</p>
+    <Card>
+      <div>
+        <div className="mainContainer">
+          <div className="container">
+            {dishes.map(buildMenu)}
+            <div className="card-info-menu">
+              <div className="notes">
+                <p>LA PROPINA NO ES OBLIGATORIA.</p>
+
+                <p>
+                  ACEPTAMOS PAGOS EN EFECTIVO, TARJETAS VISA, MASTER CARD Y
+                  AMERICAN EXPRESS.
+                </p>
+
+                <p>EL PAGO CON TARJETA NO GENERA NINGUNA COMISIÓN.</p>
+              </div>
             </div>
           </div>
-          
         </div>
       </div>
-
-
-    </div>
-  </Card>  
-
-
+    </Card>
   );
 }
