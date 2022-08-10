c3077d581d932087bf46762f8c55f08e23d44625
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-08-10 10:00:07 -0500

    add style

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { list as listDishes } from "../../../src/services/menus";
 import { useNavigate } from "react-router-dom";
+import MenuCard from "../../components/MenuCard";
 
 import "./Menu.css";
 
@@ -24,16 +25,14 @@ export default function Menu() {
     list();
   }, []);
 
-
-  
   // const buildMenu = (dish, index) => (
 
   // <div className="containerm">
   //   <div className="food-menu">
-  //     <div className="food" key={index}> 
+  //     <div className="food" key={index}>
   //         <div className="food-image">
   //           <img src="https://image.freepik.com/free-photo/front-view-delicious-meat-hamburger-with-red-tomatoes-dark-background_179666-19347.jpg" alt="Food" />
-  //         </div>  
+  //         </div>
 
   //           <div className="name-food-name"><h5>{dish.dishName}</h5></div>
   //           <div className="name-food-descrip">{dish.description}</div>
@@ -44,41 +43,30 @@ export default function Menu() {
   //           <div class="icons8-trash" onClick={() => navigate(`delete/${dish._id}`)}></div>
   //           </div>
 
-
-
-
-  //     </div>  
+  //     </div>
   //   </div>
   // </div>
 
-
-
-
-
   // );
 
-  
-
-
   return (
-
-
     <div className="mainContainer">
       <div className="container">
         <div className="row">
-          {dishes && dishes.map((dish) => <MenuCard dish={dish} navigate={navigate} />)}
+          {dishes &&
+            dishes.map((dish) => <MenuCard dish={dish} navigate={navigate} />)}
         </div>
-          <div className="info">
-            <p>LA PROPINA NO ES OBLIGATORIA.</p>
+        <div className="info">
+          <p>LA PROPINA NO ES OBLIGATORIA.</p>
 
-            <p>ACEPTAMOS PAGOS EN EFECTIVO, TARJETAS VISA, MASTER CARD Y AMERICAN EXPRESS.</p>
+          <p>
+            ACEPTAMOS PAGOS EN EFECTIVO, TARJETAS VISA, MASTER CARD Y AMERICAN
+            EXPRESS.
+          </p>
 
-            <p>EL PAGO CON TARJETA NO GENERA NINGUNA COMISIÓN.</p>
-          </div>
+          <p>EL PAGO CON TARJETA NO GENERA NINGUNA COMISIÓN.</p>
+        </div>
       </div>
-
-      </div>
-    
+    </div>
   );
-
 }
