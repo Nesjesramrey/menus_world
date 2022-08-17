1b3d2b2f1253fa3b4df33b2657a4f99823502ce6
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-08-17 14:39:19 -0500

    add cookies detail

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -1,114 +1,136 @@
-import './AddComment.css';
-import { useState } from 'react';
-import { Rating } from 'react-simple-star-rating';
-import { createComment as sendComment } from '../../services/menus';
-import { getIsLogeddIn, getIsUserAdmin } from '../../Auth/auth';
+import "./AddComment.css";
+import { useState } from "react";
+import { Rating } from "react-simple-star-rating";
+import { createComment as sendComment } from "../../services/menus";
+import { getIsLogeddIn, getIsUserAdmin } from "../../Auth/auth";
+
+//Cokkies
+import Cookies from "universal-cookie";
 
 export default function AddComment(id) {
-	const [canComment, setCanComment] = useState(false); //state
-	const [text, setText] = useState('');
+  const [canComment, setCanComment] = useState(false); //state
+  const [text, setText] = useState("");
 
-	//value of rating
-	const [ratingValue, setRatingValue] = useState(0);
-	
-	const [existComment, setExistComment] = useState(true);
+  // add usertype, ID and username
+  const cookies = new Cookies();
+  const userType = cookies.get("TipoUsuario");
+  const userId = cookies.get("Id");
+  const userName = cookies.get("Usuario");
+  console.log(userType, userId, userName);
 
-	//state of logins
-	const [isLogIn, setIsLogIn] = useState(null);
-	const [isUser, setUser] = useState(null);
+  //value of rating
+  const [ratingValue, setRatingValue] = useState(0);
 
-	// read cookies
-	console.log(getIsLogeddIn());
-	console.log(getIsUserAdmin());
-	//setIsLogIn(true);
-	//setUser(true);
-	//const idser = id;
-	//const userName = name;
+  const [existComment, setExistComment] = useState(true);
 
-	const handleRating = (rate) => setRatingValue(rate / 20);
+  //state of logins
+  const [isLogIn, setIsLogIn] = useState(null);
+  const [isUser, setUser] = useState(null);
 
-	console.log(ratingValue);
+  // read cookies
+  console.log(getIsLogeddIn());
+  console.log(getIsUserAdmin());
+  //setIsLogIn(true);
+  //setUser(true);
+  //const idser = id;
+  //const userName = name;
 
-	const sendData = () => {
-		let today = new Date();
+  const handleRating = (rate) => setRatingValue(rate / 20);
 
-		const data = {
-			user: 'Alan',
-			rating: ratingValue,
-			comment: text,
-			idUser: '23353',
-			date: today,
-		};
-		const Url = 'http://localhost:8000/detalle/' + id;
+  console.log(ratingValue);
 
-		fetch(Url, {
-			method: 'PATCH',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log('Success:', data);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-	};
+  const sendData = () => {
+    let today = new Date();
 
-	const addNewcomment = (msg) => {
-		//not logIn
-		if (isLogIn === false) {
-			console.error('Para agregar comentarios debes iniciar sesión');
-		} else {
-			//Is logIn
-			if (isUser === true) {
-				//only user can add comments
-				setCanComment(false);
-				setText(msg);
-				sendData();
-			} else {
-				//admins can't comment
-				console.error('No se puede agregar comentarios con sesión de administrador');
-			}
-		}
-		window.location.reload(false);
-	};
+    const data = {
+      user: "Alan",
+      rating: ratingValue,
+      comment: text,
+      idUser: "23353",
+      date: today,
+    };
+    const Url = "http://localhost:8000/detalle/" + id;
 
-	function createContent() {
-		if (canComment === false) {
-			return (
-				<button id="addComment" type="button" onClick={() => setCanComment(true)}>
-					Agrega un comentario
-				</button>
-			);
-		}
-		if (canComment === true) {
-			return (
-				<form id="formAddComment" action="/action_page.php">
-					<p id="textInstructions">Escribe un comentario sobre el platillo y deja tu puntuación</p>
-					<textarea
-						id="addNewComment"
-						name="newComment"
-						placeholder="Escribe tu reseña del platillo"
-						onChange={(e) => setText(e.target.value)}
-					></textarea>
-					<Rating
-						transition
-						onClick={handleRating}
-						ratingValue={ratingValue}
-						fillColor={'#ffd700'}
-						emptyColor={'#888888'}
-						size={50}
-					/>
-					<button id="addComment" type="button" onClick={(e) => addNewcomment(text)}>
-						Enviar comentario
-					</button>
-				</form>
-			);
-		}
-	}
+    fetch(Url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-	return <div id="addCommentSection">{createContent()}</div>;
+  const addNewcomment = (msg) => {
+    //not logIn
+    if (isLogIn === false) {
+      console.error("Para agregar comentarios debes iniciar sesión");
+    } else {
+      //Is logIn
+      if (isUser === true) {
+        //only user can add comments
+        setCanComment(false);
+        setText(msg);
+        sendData();
+      } else {
+        //admins can't comment
+        console.error(
+          "No se puede agregar comentarios con sesión de administrador"
+        );
+      }
+    }
+    window.location.reload(false);
+  };
+
+  function createContent() {
+    if (canComment === false) {
+      return (
+        <button
+          id="addComment"
+          type="button"
+          onClick={() => setCanComment(true)}
+        >
+          Agrega un comentario
+        </button>
+      );
+    }
+    if (canComment === true) {
+      return (
+        <form id="formAddComment" action="/action_page.php">
+          <p id="textInstructions">
+            Escribe un comentario sobre el platillo y deja tu puntuación
+          </p>
+          <textarea
+            id="addNewComment"
+            name="newComment"
+            placeholder="Escribe tu reseña del platillo"
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+          <Rating
+            transition
+            onClick={handleRating}
+            ratingValue={ratingValue}
+            fillColor={"#ffd700"}
+            emptyColor={"#888888"}
+            size={50}
+          />
+          <button
+            id="addComment"
+            type="button"
+            onClick={(e) => addNewcomment(text)}
+          >
+            Enviar comentario
+          </button>
+        </form>
+      );
+    }
+  }
+
+  return <div id="addCommentSection">{createContent()}</div>;
 }
