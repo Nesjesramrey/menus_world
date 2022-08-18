95d233a775cfce36446d06f8550305a926d8626f
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-08-18 18:43:12 -0500

    hide button QR

--- src/components/QrCode/index.jsx
+++ src/components/QrCode/index.jsx
@@ -3,6 +3,9 @@ import QRCode from "qrcode";
 
 import "./qrCode.css";
 
+//Cokkies for use name of restaurante and user category
+import Cookies from "universal-cookie";
+
 const QrCode = () => {
   const [imageUrl, setImageUrl] = useState("");
   const currentUrl = window.location.href;
@@ -16,9 +19,19 @@ const QrCode = () => {
     }
   };
 
+  const cookies = new Cookies();
+  const userType = cookies.get("TipoUsuario");
+
   return (
     <div className="container">
-      <button className="custom-btn" onClick={() => generateQrCode()}>
+      <button
+        className={`${
+          !userType || userType === "Comensal"
+            ? "custom-btn d-none"
+            : "custom-btn active"
+        }`}
+        onClick={() => generateQrCode()}
+      >
         {" "}
         Genera tu QR{" "}
       </button>
