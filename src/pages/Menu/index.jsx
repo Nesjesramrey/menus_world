c526d43439ed62055db51257b66d13b55923a5e0
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-08-19 13:44:05 -0500

    style menu

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -39,43 +39,47 @@ export default function Menu() {
   const userType = cookies.get("TipoUsuario");
 
   return (
-    <div className="mainContainer">
-      <h1 className="titleRestaurant">{`${
+    <div>
+      <h1 className="titleRestaurant ">{`${
         restaurantName === "undefined"
           ? "Bienvenido busca tu menu "
           : restaurantName
       }`}</h1>
-      <div className="container-btn-form-1 d-flex justify-content-end mb-2 mt-1 me-3">
-        <button
-          className={`${
-            !userType || userType === "Comensal"
-              ? "btn-form-1 d-none"
-              : "btn-form-1 active"
-          }`}
-          onClick={() => navigate(`/formulario`)}
-        >
-          Ir a registrar platillos
-        </button>
-        <div>
-          <QrCode />
+      <div className="mainContainer d-flex flex-wrap">
+        <div className="container-btn-form-1 d-flex justify-content-end mb-2 mt-1 me-3">
+          <button
+            className={`${
+              !userType || userType === "Comensal"
+                ? "btn-form-1 d-none"
+                : "btn-form-1 active"
+            }`}
+            onClick={() => navigate(`/formulario`)}
+          >
+            Ir a registrar platillos
+          </button>
+          <div>
+            <QrCode />
+          </div>
         </div>
-      </div>
-      
-      <div className="container">
-        <div className="row">
-          {dishes &&
-            dishes.map((dish) => <MenuCard dish={dish} navigate={navigate} />)}
+
+        <div className="container  col-md-12">
+          <div className="row col-md-12">
+            {dishes &&
+              dishes.map((dish) => (
+                <MenuCard dish={dish} navigate={navigate} />
+              ))}
+          </div>
         </div>
-      </div>
 
-      <div className="info">
-        <p>LA PROPINA NO ES OBLIGATORIA.</p>
-        <p>
-          ACEPTAMOS PAGOS EN EFECTIVO, TARJETAS VISA, MASTER CARD Y AMERICAN
-          EXPRESS.
-        </p>
+        <div className="info">
+          <p>LA PROPINA NO ES OBLIGATORIA.</p>
+          <p>
+            ACEPTAMOS PAGOS EN EFECTIVO, TARJETAS VISA, MASTER CARD Y AMERICAN
+            EXPRESS.
+          </p>
 
-        <p>EL PAGO CON TARJETA NO GENERA NINGUNA COMISIÓN.</p>
+          <p>EL PAGO CON TARJETA NO GENERA NINGUNA COMISIÓN.</p>
+        </div>
       </div>
     </div>
   );
