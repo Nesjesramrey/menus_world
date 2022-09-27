9477ffc5a8f156b2c906541cb1f72cf3c01eb862
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-09-27 17:10:29 -0500

    add comments

--- src/components/QrCode/index.jsx
+++ src/components/QrCode/index.jsx
@@ -1,51 +1,55 @@
-import React, { useState } from 'react';
-import QRCode from 'qrcode';
+import React, { useState } from "react";
+import QRCode from "qrcode";
 
-import './qrCode.css';
+import "./qrCode.css";
 
 //Cokkies for use name of restaurante and user category
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 
 const QrCode = () => {
-	const [imageUrl, setImageUrl] = useState('');
-	const currentUrl = window.location.href;
-	const generateQrCode = async () => {
-		try {
-			const response = await QRCode.toDataURL(currentUrl, { width: 200 });
-			setImageUrl(response);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const [imageUrl, setImageUrl] = useState("");
+  const currentUrl = window.location.href;
+  const generateQrCode = async () => {
+    try {
+      const response = await QRCode.toDataURL(currentUrl, { width: 200 });
+      setImageUrl(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const cookies = new Cookies();
-	const userType = cookies.get('TipoUsuario');
+  const cookies = new Cookies();
+  const userType = cookies.get("TipoUsuario");
 
-	function renderQr() {
-		if (userType) {
-			if (userType === 'Administrador de restaurante') {
-				return (
-					<div className="container qrSection">
-						<div>
-							<button className="custom-btn ms-auto" onClick={() => generateQrCode()}>
-								Genera tu QR
-							</button>
-						</div>
-						<div>
-							{imageUrl ? (
-								<a href={imageUrl} download>
-									<img src={imageUrl} alt="img" />
-								</a>
-							) : null}
-						</div>
-					</div>
-				);
-			}
-		}
-	}
-	const qr = renderQr();
+  function renderQr() {
+    if (userType) {
+      if (userType === "Administrador de restaurante") {
+        return (
+          <div className="container qrSection">
+            <div>
+              <button
+                className="custom-btn ms-auto"
+                onClick={() => generateQrCode()}
+              >
+                Genera tu QR
+              </button>
+            </div>
 
-	return <div>{qr}</div>;
+            <div>
+              {imageUrl ? (
+                <a href={imageUrl} download>
+                  <img src={imageUrl} alt="img" />
+                </a>
+              ) : null}
+            </div>
+          </div>
+        );
+      }
+    }
+  }
+  const qr = renderQr();
+
+  return <div>{qr}</div>;
 };
 
 export default QrCode;
